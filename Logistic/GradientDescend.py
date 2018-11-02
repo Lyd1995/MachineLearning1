41cb42aa8cb35a36c67ab522329692f2ae5e1e72
Ceveraise <842354018@qq.com>
2018-11-02 10:18:35 +0800

    Update GradientDescend.py

--- Logistic/GradientDescend.py
+++ Logistic/GradientDescend.py
@@ -7,26 +7,30 @@ NUM_CHARACTER = 2
 alpha = 0.01
 
 def main():
+
+    SEED = 23455
+    # 基于seed产生随机数
+    rdm = np.random.RandomState(SEED)
     # X为输入训练集，有NUM_TRAIN个训练样本,每个样本有NUM_CHARATER个特征
     # Y是标签
-    X = np.random.rand(NUM_TRAIN, NUM_CHARACTER)
-    Y = [[int((x1 + x2) < 0.7)] for (x1, x2) in X]
+    X = rdm.rand(NUM_TRAIN, NUM_CHARACTER)
+    Y = [[int((x1 + x2) < 0.6)] for (x1, x2) in X]
 
     # 参数w和b
-    w = np.random.rand(NUM_CHARACTER)
+    w = rdm.rand(NUM_CHARACTER)
     b = 1
 
     # 开始使用梯度下降算法,一次迭代过程：
-    dw = [0] * NUM_CHARACTER
-    db = 0
+    dw = [0] * NUM_CHARACTER   # dw 表示 dJ/dw
+    db = 0                      # db 表示 dJ/db
     for i in range(NUM_TRAIN):
         z = np.dot(w, X[i]) + b  # z = wx + b
         a = 1/(1 + math.exp(-z))  # 计算预测值
-        db = db + dz
         dz = a - Y[i][0]  # dz 表示 dJ/dz
+        db = db + dz  # db = dJ/db
         for j in range(NUM_CHARACTER):
             dw[j] = dw[j] + X[i][j] * dz
-            
+
 
     db = db / NUM_TRAIN
     b = b - alpha * db
@@ -39,5 +43,4 @@ def main():
     print(b)
 
 main()
-
 
